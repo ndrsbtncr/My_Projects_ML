289411ce293ccb251c7692ce05a3cbf8a42447c8
ndrsbtncr <60939710+ndrsbtncr@users.noreply.github.com>
2024-03-06 08:49:04 -0500

    Add files via upload

---
+++ Ejemplos/EjemploAPI_GPT.py
@@ -0,0 +1,16 @@
+import openai
+
+# Configura tu clave API de OpenAI
+openai.api_key = "Escribe aqui el token"
+
+# Texto de entrada para generar continuación
+input_text = '¿Quién es Leonel Messi?'
+
+chat_completion = openai.ChatCompletion.create(
+    model = "gpt-4-0125-preview",
+    messages=[{"role":"user","content":input_text}]
+)
+
+response = chat_completion['choices'][0]['message']['content']
+
+print(response)
